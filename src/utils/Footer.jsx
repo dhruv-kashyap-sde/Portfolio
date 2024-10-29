fe129c82fdbab9bfcd86351fef271be1b8c30254
Dhruv Kashyap <dhruv.kashyap1111@gmail.com>
2024-10-29 12:59:21 +0530

    finalized the footer

--- src/utils/Footer.jsx
+++ src/utils/Footer.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import './Footer.css'
+import FooterAnimation from './animations/FooterAnimation'
 
 const Footer = () => {
+    FooterAnimation();
   return (
+    <>
     <div className='footer-container main-text'>
-        <div className="box-links">
-            <a href="home">Home</a>
-            <a href="project">Projects</a>
-            <a href="about">About</a>
-            <a href="contact">Contact</a>
+        <div className="footer-box-container">
+            <div className="box-links">
+                <a href="home">Home</a>
+                <a href="project">Projects</a>
+                <a href="about">About</a>
+                <a href="contact">Contact</a>
+            </div>
+            <div className='box-logo'>
+                <p className="logo">D K</p>
+            </div>
+            <div className="box-follow">
+                <i class="ri-instagram-line"></i>
+                <i class="ri-github-fill"></i>
+                <i class="ri-linkedin-box-fill"></i>
+            </div>
         </div>
-        <div className='box-logo'>
-            <p className="logo">Dhruv Kashyap</p>
-        </div>
-        <div className="box-follow">
-            <i class="ri-instagram-line"></i>
-            <i class="ri-github-fill"></i>
-            <i class="ri-linkedin-box-fill"></i>
+        <div className="main-text footer-bottom">
+            <p className='footer-text'>
+                <span><i class="ri-copyright-line"></i> 2025</span>
+                <span>Dhruv Kashyap</span>
+                <span>All Rights Reserved.</span>
+            </p>
         </div>
     </div>
+    </>
   )
 }
 
---
+++ src/utils/animations/FooterAnimation.js
@@ -0,0 +1,33 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React from 'react'
+
+const FooterAnimation = () => {
+
+    useGSAP(() => {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.footer-container',
+                markers: true,
+                start: 'top 70%',
+                end: '40% 70%',
+                scrub:1
+
+            }
+        })
+        tl.from('.box-links, .box-logo, .box-follow', {
+            y:300,
+            ease: 'power3',
+            duration:2,
+            opacity: 0
+        })
+        tl.from('.footer-text', {
+            opacity:1,
+            duration:1,
+            ease: 'power2'
+        })
+      
+    })
+}
+
+export default FooterAnimation
